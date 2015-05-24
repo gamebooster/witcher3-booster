3260bcdc57d61e41d6e802a2dcde28b10a64c083
Karl Skomski <mail@skomski.com>
2015-05-24 23:04:49 +0200

    Use via dsound proxy

--- main.cc
+++ main.cc
@@ -5,28 +5,35 @@
 
 HANDLE thread = nullptr;
 utils::VtableHook* game_hook = nullptr;
-void* global_debug_console = nullptr;
+void** global_debug_console = nullptr;
 
 typedef bool(*OnViewportInputType) (void* thisptr, void* viewport, int input_key, int input_action, float tick);
 OnViewportInputType OnViewportInputDebugConsole = nullptr;
 
 bool OnViewportInputDebugAlwaysHook(void* thisptr, void* viewport, int input_key, int input_action, float tick) {
-  return OnViewportInputDebugConsole(global_debug_console, viewport, input_key, input_action, tick);
+  return OnViewportInputDebugConsole(*global_debug_console, viewport, input_key, input_action, tick);
 }
 
 DWORD WINAPI InitializeHook(void* arguments) {
 	hook::set_base();
 
 	char* location = hook::pattern("48 8B 05 ? ? ? ? 48 8D 4C 24 ? C6 44 24").count(1).get(0).get<char>(3);
-	void* global_game = *reinterpret_cast<void**>(location + *(int32_t*)location + 4);
+	void** global_game = reinterpret_cast<void**>(location + *(int32_t*)location + 4);
 
 	location = hook::pattern("48 89 05 ? ? ? ? EB 07 48 89 35 ? ? ? ? 48 8B 47 60").count(1).get(0).get<char>(3);
-	global_debug_console = *reinterpret_cast<void**>(location + *(int32_t*)location + 4);
+	global_debug_console = reinterpret_cast<void**>(location + *(int32_t*)location + 4);
+
+	while (*global_game == nullptr || *global_debug_console == nullptr) {
+		OutputDebugStringW(L"Wait for game");
+		Sleep(500);
+	}
 
 	location = hook::pattern("48 83 EC 28 48 8B 05 ? ? ? ? 0F B6 90 ? ? ? ?").count(1).get(0).get<char>(0);
 	OnViewportInputDebugConsole = (OnViewportInputType)location;
 
-	game_hook = new utils::VtableHook(global_game);
+	OutputDebugStringW(L"Hook game");
+
+	game_hook = new utils::VtableHook(*global_game);
 	game_hook->HookMethod(OnViewportInputDebugAlwaysHook, 128);
 
 	return 1;
